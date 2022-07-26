2e9fec6620ceb782c3405f20a10362b0caeb12e9
Blakdothraki <blakdothraki@github.com>
2022-07-26 19:05:35 +0100

    Finalizing

--- app/src/main/java/com/softwork/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/softwork/ageinminutes/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.softwork.ageinminutes
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import java.util.*
 
 
@@ -16,41 +15,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val dateBtn = findViewById<Button>(R.id.dateBtn)
-
-        dateBtn.setOnClickListener{ view ->
-            clickDatePicker(view)
-            //Toast.makeText(this, "Button Works", Toast.LENGTH_LONG).show()
-        }
-    }
-    fun clickDatePicker(view: View){
-        val myCalender = Calendar.getInstance()
-        val year = myCalender.get(Calendar.YEAR)
-        val month = myCalender.get(Calendar.MONTH)
-        val day = myCalender.get(Calendar.DAY_OF_MONTH)
-
-        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view, selectedYear, selectedMonth, selectedDayOfMonth ->
-            Toast.makeText(this, "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear", Toast.LENGTH_LONG).show()
-            val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
-
-            val selectedDateTV = findViewById<TextView>(R.id.selectedDateTV)
-            selectedDateTV.setText(selectedDate)
-            
-            val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
-            val parsedDateFormat = simpleDateFormat.parse(selectedDate)
-            val selectedDateInMinutes = parsedDateFormat!!.date / 60000
-            val parsedCurrDate = simpleDateFormat.parse(simpleDateFormat.format(System.currentTimeMillis()))
-            val currDateInMinutes = parsedCurrDate!!.date / 60000
-            val diffInMinutes = currDateInMinutes - selectedDateInMinutes
-
-            val selectedDateInMinutesTV = findViewById<TextView>(R.id.selectedDateInMinutesTV)
-            selectedDateInMinutesTV.setText(diffInMinutes.toString())
-
-
-                                                                 }, year, month, day)
-        dpd.datePicker.setMaxDate(Date().time - 86400000)
-        dpd.show()
     }
 
+    fun openDateTimePicker(view: View) {
+        val c = Calendar.getInstance()
+
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener { datePicker, yyyy, MM, dd ->
+            var dt = "$dd/${MM + 1}/$yyyy"
+            val editDateAndTime = findViewById<TextView>(R.id.editDateAndTime)
+            //editDateAndTime.setText(dt)
+            TimePickerDialog(this, TimePickerDialog.OnTimeSetListener { timePicker, HH, Min -> dt += " $HH:$Min"
+            editDateAndTime.setText(dt)
+            }, c.get(Calendar.HOUR), c.get(Calendar.MINUTE), false).show()
+
+        }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)).show()
+    }
+
+   fun calculateAge(view: View) {
+        var today = Date()
+       val editDateAndTime = findViewById<TextView>(R.id.editDateAndTime)
+        var dobs = editDateAndTime.text.toString()
+        var sdf = SimpleDateFormat("dd/MM/yyyy HH:mm")
+        var parsedDob: Date = sdf.parse(dobs)
+        var days = (today.time - (parsedDob.time))/86_400_000
+        var hours = (today.time - parsedDob.time)%86_400_000/3_600_000
+        var minutes = (today.time - parsedDob.time)%86_400_000%3_600_000/60_000
+        var secs = (today.time - parsedDob.time)%86_400_000%3_600_000%60_000/1_000
+
+       val textView = findViewById<TextView>(R.id.textView)
+       textView.visibility = View.VISIBLE
+        textView.setText("Days = $days\nHours = $hours\nMinutes = $minutes\nSeconds = $secs")
+    }
 }
